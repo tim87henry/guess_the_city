b0b26a0c29704efa8964ddea2983be5f45502305
Timothy <tim87henry@gmail.com>
2020-09-14 23:02:15 +1000

    Modified page layout and color scheme

--- score.js
+++ score.js
@@ -1,10 +1,12 @@
 const btnScore = document.querySelector('#score');
+var listOfQ=document.getElementsByClassName("question")
+var numQuestions=listOfQ.length
 
 btnScore.addEventListener('click', function (e) {
     points=0
     answers=[1,3,0,0,2,0,3,1]
     
-    for (let i=0;i<8;i++) {
+    for (let i=0;i<numQuestions;i++) {
         var question=document.getElementsByName("question"+(i+1))
         if (question[answers[i]].checked) {
             points+=1
@@ -12,3 +14,24 @@ btnScore.addEventListener('click', function (e) {
     }
     alert("You scored "+points+" points")
 });
+
+var currentQ=1
+showQuestion(currentQ);
+
+function navigate(num) {
+    currentQ=currentQ+num;
+    showQuestion(currentQ);
+}
+
+function showQuestion(n) {
+    if (n>numQuestions) {
+        n=1
+    } else if (n<1) {
+        n=numQuestions
+    }
+    for (let i=0;i<numQuestions;i++) {
+        listOfQ[i].style.display="none";
+    }
+    listOfQ[n-1].style.display="block";
+    currentQ=n
+}
